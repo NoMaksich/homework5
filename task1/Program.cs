09234cd51341a453b3bbb2114745c1efddbafe5c
maximmetalnikov <maxummetalnikov@mail.ru>
2023-05-25 18:48:12 +0300

    Задани 1 выполнено

---
+++ task1/Program.cs
@@ -0,0 +1,27 @@
+﻿int[] Function(int[] array)
+{
+    for(int i=0; i<8; i++)
+    {
+        array[i]=new Random().Next(100,1000);
+    }
+    return array;
+}
+
+int[] array=new int[8];
+array=Function(array);
+Console.Write("Значения массива: ");
+for(int i=0; i<8; i++)
+{
+    Console.Write(array[i]+" ");
+}
+Console.WriteLine("");
+Console.Write("Количество четных чисел: ");
+int count=0;
+for(int i=0; i<8; i++)
+{
+    if(array[i]%2==0)
+    {
+        count++;
+    }
+}
+Console.Write(count);
